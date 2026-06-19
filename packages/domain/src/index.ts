b1d5fb32e52b87e17f9738c73a8aebee99f2d0f8
mirajob <dev@mirajob.cloud>
2026-06-19 23:28:41 +0200

    fix: export missing constants from domain package

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,3 +1,4 @@
 export { ALLOWED_STUDENT_DOMAINS, validateStudentEmail } from "./validation";
 export { ASSOCIATION_PERMISSIONS, ROLE_PERMISSION_TEMPLATES } from "./permissions";
-export { ASSOCIATION_CATEGORIES, APPLICATION_STATUS_LABELS } from "./constants";
+export { ASSOCIATION_CATEGORIES, APPLICATION_STATUS_LABELS, FIT_CATEGORY_LABELS, INVITATION_EXPIRY_DAYS } from "./constants";
+export type { AssociationPermission } from "./permissions";
